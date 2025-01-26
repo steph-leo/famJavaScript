161500ead7aa8cc157b537c4d04477f2e9186c98
steph-leo <leostephs89@gmail.com>
2025-01-27 00:23:52 +0300

    Add files

---
+++ give.js
@@ -0,0 +1,10 @@
+function great(name) {
+    console.log('Hey ' + name + ' answer is:');
+};
+
+function square(number) {
+    return number * number;
+};
+let number = square(2);
+great("Stephano");
+console.log(number);
